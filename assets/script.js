7bf22948d93093a6481ed54b6ac03c2339c8a058
MouawiyyaShafiq <mouawiyya.sh@hotmail.com>
2024-12-16 10:54:19 +0100

    Étape 2 : Ajoutez des Event Listeners sur les flèches

--- assets/script.js
+++ assets/script.js
@@ -16,3 +16,18 @@ const slides = [
 		"tagLine":"Autocollants <span>avec découpe laser sur mesure</span>"
 	}
 ]
+
+//Modif : ajout des eventlistener sur les flèches//
+
+let arrowLeft = document.querySelector(".arrow_left");
+let arrowRight = document.querySelector(".arrow_right");
+
+arrowLeft.addEventListener("click", () =>{
+	console.log("flèche gauche clicked")
+})
+
+arrowRight.addEventListener("click", () =>{
+	console.log("flèche droite clicked")
+})
+
+//---------------------------------------------//
